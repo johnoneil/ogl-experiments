5c05b959a0d56e13678eb97f39a7923c642aa7ad
johnoneil <oneil.john@gmail.com>
2020-05-16 14:01:53 -0700

    Brought in tweeny for tween subsystem

---
+++ include/framework/tween.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "framework/isubsystem.h"
+
+class TweenSystem : public iSubsystem
+{
+public:
+    TweenSystem();
+    TweenSystem(const TweenSystem& other) = delete;
+    TweenSystem& operator=(const TweenSystem& other) = delete;
+    ~TweenSystem();
+public:
+    bool Startup() override;
+    bool Shutdown() override;
+    void Update(const float dt) override;
+    void Render() override;
+};
---
+++ src/framework/tween.cpp
@@ -0,0 +1,35 @@
+#include "framework/tween.h"
+
+TweenSystem::TweenSystem() {
+
+}
+
+#if 0
+TweenSystem::TweenSystem(const TweenSystem& other) {
+
+}
+
+TweenSystem& TweenSystem::operator=(const TweenSystem& other) {
+
+}
+#endif
+
+TweenSystem::~TweenSystem() {
+
+}
+
+bool TweenSystem::Startup() {
+    return true;
+}
+
+bool TweenSystem::Shutdown() {
+    return true;
+}
+
+void TweenSystem::Update(const float dt) {
+
+}
+
+void TweenSystem::Render() {
+
+}
